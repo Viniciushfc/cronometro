1339c5cdaba9b47db95337515d88c4d576a525c3
Vinicius Henry Ferrari Campos <89172416+Viniciushfc@users.noreply.github.com>
2022-05-06 13:14:30 -0300

    Crônometro

---
+++ cronometro/script.js
@@ -0,0 +1,43 @@
+var sec = 0
+var min = 0
+var hr = 0
+var interval
+
+
+function start(){
+    watch()
+    interval = setInterval(watch, 1000)
+}
+
+function pause(){
+    clearInterval(interval)
+}
+
+function stopp(){
+    clearInterval(interval)
+    sec = 0 
+    min = 0
+
+    document.getElementById('watch').innerText = "00:00:00"
+}
+
+function watch(){
+    sec++
+    if(sec == 60){
+        min++
+        sec = 0
+        if(min = 60){
+            min=0
+            hr ++
+        }
+    }
+    document.getElementById('watch').innerText = zero(hr) + ':' + zero(min) + ':' + zero(sec)
+}
+
+function zero (digit){
+    if(digit <10){
+        return ('0'+ digit)
+    }else{
+        return digit
+    }
+}
